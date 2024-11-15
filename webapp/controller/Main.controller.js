a566bde53baccf13a2f7f6ef9bf482c28482196d
Emre AYDIN <emreaydin1129@gmail.com>
2024-11-15 14:53:53 +0300

    .

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -5,7 +5,13 @@ sap.ui.define(["./BaseController",
     'sap/ui/core/Fragment',
     'sap/ui/Device',
     'sap/ui/model/Sorter'
-], function (BaseController, Filter, FilterOperator, Element, Fragment,Device, Sorter) {
+], function (BaseController,
+	Filter,
+	FilterOperator,
+	Element,
+	Fragment,
+	Device,
+	Sorter) {
     "use strict";
 
     return BaseController.extend("com.eren.stocktrans.controller.Main", {
@@ -20,9 +26,7 @@ sap.ui.define(["./BaseController",
 
         onInit: function () {
             this.getRouter().getRoute("RouteMain").attachPatternMatched(this._onObjectMatched, this);
-            var that = this;
-
-
+            this.currentPage = 1;
         },
 
         /* =========================================================== */
@@ -37,8 +41,6 @@ sap.ui.define(["./BaseController",
                 SNS: aSelectedContexts[0].getObject().SnsNo
             });
             oViewModel.setProperty("/SelectedObject", aSelectedContexts[0].getObject());
-
-
         },
 
         /* =========================================================== */
@@ -49,7 +51,10 @@ sap.ui.define(["./BaseController",
                 oTable = this.getView().byId("idTable");
             oTable.removeSelections(true);
 
-            this.getAllData();
+         
+            this.pageSize = 20;       
+            this.loadPageData();           
+            
 
         },
 
@@ -90,7 +95,6 @@ sap.ui.define(["./BaseController",
             // temparray.push(Filters);
 
             // this.getView().getModel("viewModel").setProperty("/Filters", temparray);
-
         },
 
 
@@ -99,6 +103,19 @@ sap.ui.define(["./BaseController",
             var oTable = this.getView().byId("idTable");
 
             oTable.getBinding("items").filter(oFilter);
+
+            let aAllData =  this.getView().getModel("viewModel").getProperty("/allData");
+			let aFilteredData = [];
+			if (oFilter.aFilters) {
+				 aFilteredData = aAllData.filter(item => {
+                return this._applyCustomFilter(oFilter, item);
+            });
+			}
+			
+            this.getView().getModel("viewModel").setProperty("/FilteredData", aFilteredData);
+			this.currentPage = 1;
+			this.updateTable();
+
         },
 
         handleFacetFilterReset: function (oEvent) {
@@ -147,12 +164,15 @@ sap.ui.define(["./BaseController",
         },
 
 
-        getAllData: async function () {
-            
-            let oViewModel = this.getModel("viewModel");
+        loadPageData: async function () {
+                let oViewModel = this.getModel("viewModel");
 
                 let fnSuccess = (oData) => {
-                    oViewModel.setProperty("/tableListForFilter", oData.results);
+                    //tüm datayı çek.
+                    oViewModel.setProperty("/allData", oData.results);
+                    oViewModel.setProperty("/totalCount", oData.results.length);
+                    this.allData = oData.results; // Tüm veriyi sakla
+                    this.updateTable();
                 },
                 fnError = (err) => { },
                 fnFinally = () => {
@@ -167,8 +187,51 @@ sap.ui.define(["./BaseController",
 
         },
 
-        _getAllData: async function () {
+        updateTable: function () {
+            const start = (this.currentPage - 1) * this.pageSize;
+            const end = start + this.pageSize;
+            const oTableModel = this.getView().getModel("viewModel");
+            let filteredData =  oTableModel.getProperty("/FilteredData");
+            let pageData = [];
+            
+            if(filteredData.length > 0){
+                pageData = filteredData.slice(start, end);
+            }else{
+                //eğer filtrelenmişse ekran, filtrenmiş data üzerinden slice yap
+                pageData = this.allData.slice(start, end);
+            }
+            
+            oTableModel.setProperty("/displayedData", pageData);        
+            this.getView().getModel("viewModel").setProperty("/currentPage", this.currentPage);
+            this.getView().getModel("viewModel").setProperty("/start", start+1);
+            this.getView().getModel("viewModel").setProperty("/end", start + pageData.length);
+        },
+        
 
+        onNextPage: function () {
+            let viewModel =  this.getView().getModel("viewModel");
+            let totalCount = viewModel.getProperty("/totalCount"),
+                filteredData = viewModel.getProperty("/FilteredData")
+            //eğer daha önce filtre kullanıldıysa, tüm data üzerinden değilde filtre üzreinden bir next olsun
+    
+            if(filteredData.length > 0){
+                totalCount = filteredData.length;
+            }
+            
+            if ((this.currentPage * this.pageSize) < totalCount) {
+                this.currentPage++;
+                this.updateTable();
+            }
+        },
+        
+        onPreviousPage: function () {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+                this.updateTable();
+            }
+        },
+
+        _getAllData: async function () {
             let oModel = this.getView().getModel(),
                 sPath = "/ListSet",
                 aFilters = [];
@@ -176,19 +239,24 @@ sap.ui.define(["./BaseController",
             return new Promise((fnResolve, fnReject) => {
                 let oParams = {
                     filters: aFilters,
-                    success: fnResolve,
-                    error: fnReject,
+                    success: function (oData) {
+                        fnResolve(oData);
+                    },
+                    error: function (oError) {
+                        fnReject(oError); 
+                    }
                 };
+        
+                // Veriyi modelden oku
                 oModel.read(sPath, oParams);
             });
-
         },
 
         _updateFilters: function () {
             let oViewModel = this.getView().getModel("viewModel");
         
             // /tableListForFilter içerisindeki veriyi al
-            let tableData = oViewModel.getProperty("/tableListForFilter");
+            let tableData = oViewModel.getProperty("/allData");
         
             if (!tableData) {
                 console.warn("Filter oluşturmak için veri bulunamadı.");
@@ -215,6 +283,38 @@ sap.ui.define(["./BaseController",
         
             // Filters modeline yükle
             oViewModel.setProperty("/Filters", [Filters]);
+        },
+        _applyCustomFilter: function (oFilter, item) {
+            if (!oFilter) {
+                return true; // Eğer filtre yoksa, tüm öğeleri döndür
+            }
+        
+            if (oFilter.aFilters) {
+                // AND/OR için birden fazla filtre
+                return oFilter.aFilters.every(subFilter => this._applyCustomFilter(subFilter, item));
+            }
+        
+            const { sPath, sOperator, oValue1 } = oFilter;
+            const value = item[sPath]; 
+        
+            switch (sOperator) {
+                case "EQ": 
+                    return value === oValue1;
+                case "NE": 
+                    return value !== oValue1;
+                case "GT": 
+                    return value > oValue1;
+                case "GE": 
+                    return value >= oValue1;
+                case "LT": 
+                    return value < oValue1;
+                case "LE": 
+                    return value <= oValue1;
+                case "Contains": 
+                    return value && value.includes(oValue1);
+                default:
+                    return true; 
+            }
         }
     });
 });
